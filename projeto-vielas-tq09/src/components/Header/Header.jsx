6bc20517c2516b0cf8a84e0a07b993ea1d91b6e2
Miih Morais <operacionalmoraisefilhos@gmail.com>
2024-03-21 13:29:39 -0300

    Adicionado barra de navegação e css

--- projeto-vielas-tq09/src/components/Header/Header.jsx
+++ projeto-vielas-tq09/src/components/Header/Header.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import './header.css'
 
 const Header = () => {
   return (
-    <div>Header componente da Thamires</div>
-  )
+    <div className='Header'>
+      <ul className='ul-header'>
+        <li>
+          <a href="/">
+           <img src="./Image/menu.png" alt="Menu" />
+           </a>
+          </li>
+          <li class='name'>
+            <a href="/">Vielas</a>
+          </li>
+          <li id='login' button='login'>
+            <a href="/login">Login</a>
+          </li>
+          <li id='perfil'>
+            <a href="/perfil">Foto Perfil</a>
+          </li>
+        
+      </ul>
+    </div>
+  );
 }
 
-export default Header
+const Busca = () => {
+  return (
+    <div classNme='busca'>
+      <img src="./Image/lupa.png" alt="Buscar" />
+      <input type="text" placeholder="Buscar" />
+    </div>
+  );
+}
+
+export default Header;
