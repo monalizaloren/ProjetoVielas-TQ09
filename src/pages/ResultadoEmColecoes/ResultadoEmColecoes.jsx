11fc55c9247eac5cbda7c519d0f4e437d932b2b6
Romeu maier <romeumaier@gmail.com>
2024-04-19 13:56:16 -0300

    estrutura componente

--- src/pages/ResultadoEmColecoes/ResultadoEmColecoes.jsx
+++ src/pages/ResultadoEmColecoes/ResultadoEmColecoes.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 const ResultadoEmColecoes = () =>{
     return(
-
+        <section>
+            <h2> Resultado em Coleções</h2>
+            <div>                
+                <picture>
+                    <h2> Resultado em coleções</h2>
+                    <img src="#" alt="imagem descritiva do nosso produto escolhido">
+                    </img>
+                </picture>
+                <div>
+                    <h3> Crise Financeira </h3>
+                    <p> Como a alta dos preços impacta as famílias das periferias.</p>
+                </div>
+            </div>
+            <div>
+                <h3> Outros resultados </h3>
+                < FotosSemelhantes />
+            </div>
+        </section>
     )
 }
 export default ResultadoEmColecoes;
