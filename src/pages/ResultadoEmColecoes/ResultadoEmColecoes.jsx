339a420e54d101bd0e6610cac18dd862ae4fbda0
Romeu maier <romeumaier@gmail.com>
2024-04-24 14:11:53 -0300

    estrutura o layout

--- src/pages/ResultadoEmColecoes/ResultadoEmColecoes.jsx
+++ src/pages/ResultadoEmColecoes/ResultadoEmColecoes.jsx
@@ -1,24 +1,29 @@
 import React from 'react';
-
+import resultadoImage from '../../images/51415802546_c8275329a9_k.png'
+import Header from '../../components/Header/Header';
+import FotosSemelhantes from '../../components/FotosSemelhantes/FotosSemelhantes';
+import Footer from '../../components/Footer/Footer';
 const ResultadoEmColecoes = () =>{
     return(
         <section className='sessao'>
+            <Header/ >
             <h2> Resultado em Coleções</h2>
             <div>                
-                <picture className='caixaesquerda'>
+                <picture className='caixaEsquerda'>
                     <h2> Resultado em coleções</h2>
-                    <img src="#" alt="imagem descritiva do nosso produto escolhido">
+                    <img src={resultadoImage} className='imageCaixa' alt="imagem descritiva do nosso produto escolhido">
                     </img>
                 </picture>
-                <div className='caixadireita'>
+                <div className='caixaDireita'>
                     <h3> Crise Financeira </h3>
                     <p> Como a alta dos preços impacta as famílias das periferias.</p>
                 </div>
             </div>
             <div>
                 <h3> Outros resultados </h3>
-                < FotosSemelhantes />
+                <FotosSemelhantes />
             </div>
+            <Footer />
         </section>
     )
 }
