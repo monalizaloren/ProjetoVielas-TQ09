250a48e4ddf33a00c9c0c222a5af4d6d5c3763cc
DeividsonOmedio <deividsonomedio@outlook.com>
2024-04-29 17:57:50 -0300

    Banner Refatorado e criado componentes filhos

---
+++ src/components/BannerCard/BannerCard.jsx
@@ -0,0 +1,16 @@
+import "./BannerCard.css"
+
+const BannerCard = () => {
+    return (
+        <div className='texto'>
+                <h1>VIELAS</h1>
+
+                <p className='paragrafo'>Um banco de imagens pensado para mostrar a diversidade das periferias de São Paulo.
+                    <br />
+                    Nascido nas quebradas e fomentado por quem vive e pensa nelas.
+                </p>
+            </div>
+    )
+}
+
+export default BannerCard;
---
+++ src/components/BannerCarousel/BannerCarousel.jsx
@@ -0,0 +1,12 @@
+import "./BannerCarousel.css"
+import img01carrocel from '../../images/pauta-com-o-ngks-no-jardim-amalia-capao-redondo---zona-sul---sp2021_leu-britto-20_50998249707_o.jpg'
+
+const BannerCarousel = () => {
+    return (
+        <div className='BannerCarousel'>
+            <img src={img01carrocel} className="" alt="." />    
+        </div>
+    )
+}
+
+export default BannerCarousel;
