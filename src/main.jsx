8c07b4ab28c2d9ddf69e21fdb2ce7c969e4168a1
Alisson Oliveira <devalissonoliveira@gmail.com>
2024-05-15 10:47:56 -0300

    Instalado o React Router Dom e o Styled Components e criado os styles globais

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import GlobalStyle from "./GlobalStyle";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GlobalStyle />
+  </React.StrictMode>
+);
