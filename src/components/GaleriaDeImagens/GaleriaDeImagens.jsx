d2038cc2e54767482f8a4683aef9ac64bf7926f5
Alisson Oliveira <devalissonoliveira@gmail.com>
2024-05-15 08:47:34 -0300

    Deletado todo projeto antigo e instalado o VITE

--- src/components/GaleriaDeImagens/GaleriaDeImagens.jsx
+++
@@ -1,7 +0,0 @@
-import "./GaleriaDeImagens.css";
-
-const GaleriaDeImagens = ({ children }) => {
-  return <section className="galeria-imagens">{children}</section>;
-};
-
-export default GaleriaDeImagens;
--- src/components/ImagemItem/ImagemItem.jsx
+++
@@ -1,7 +0,0 @@
-import "./ImagemItem.css";
-
-const ImagemItem = ({ src, alt, classNameImg }) => {
-  return <img src={src} alt={alt} className={classNameImg} />;
-};
-
-export default ImagemItem;
