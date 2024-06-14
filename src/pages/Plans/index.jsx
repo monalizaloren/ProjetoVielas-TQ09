0face4e3a57a4c6180e0bd3f7a29522c9c5a7eff
Alisson Oliveira <devalissonoliveira@gmail.com>
2024-06-14 12:45:07 -0300

    feat(pages): Plans page.

---
+++ src/pages/Plans/index.jsx
@@ -0,0 +1,12 @@
+import PlansTable from "../../components/PlansTable";
+import Layout from "../../layout";
+
+const Plans = () => {
+  return (
+    <Layout>
+      <PlansTable />
+    </Layout>
+  );
+};
+
+export default Plans;
