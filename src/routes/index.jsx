f5139cdd04c7cb42aa10788aee8139e6bc38c631
Alisson Oliveira <devalissonoliveira@gmail.com>
2024-06-14 12:43:22 -0300

    feat(routes): Plan route.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,9 +5,15 @@ import {
 } from "react-router-dom";
 
 import HomePage from "../pages/HomePage";
+import Plans from "../pages/Plans";
 
 const routes = createBrowserRouter(
-  createRoutesFromElements(<Route path="/" element={<HomePage />} />)
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/plans" element={<Plans />} />
+    </>
+  )
 );
 
 export default routes;
